c88fca48de3815e4154f141b4044b450dfe97144
Eliza Cantwell <elizaofthebay@gmail.com>
2023-10-20 15:26:19 -0700

    Update for Heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ws#y22rxzjgv4c71itpfxjs0h*pf87pe)_lq(5b0n1j*g-ky!7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
